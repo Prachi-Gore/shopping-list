0cbe16bbd6282ea440311275d077ae0194fd772d
Prachi Gore <prachigore408@gmail.com>
2024-05-11 17:25:08 +0530

    boilerplate setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+  return (
+    <>
+      hii
+      <div className="container py-4 px-3 mx-auto">
+      <h1>Hello, Bootstrap and Vite!</h1>
+      <button className="btn btn-primary">Primary button</button>
+    </div>
+
+    </>
+  )
+}
+
+export default App
