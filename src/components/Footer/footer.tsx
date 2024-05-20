c270ccb993668c4b40f6bfc6d7dfa85dd8211e48
Prachi Gore <prachigore408@gmail.com>
2024-05-20 09:17:42 +0530

    rolling footer added

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,8 +1,8 @@
 const Footer=()=>{
-    const keyPoints=['ContextApi','Bootstrap','React','typescript','useReducer',
+    const keyPoints=['ContextApi','Bootstrap5','React','typescript','useReducer',
     'notification','react-toast','useForm','html','animation']
     return(
-        <footer className="fixed-bottom text-white moving-text d-flex gap-4 bg-green-600 px-2 py-1.5">
+        <footer className="fixed-bottom text-white moving-text d-flex gap-4 px-2 py-1.5">
             {
                 keyPoints.map((item,id)=> <p key={id} className="px-3 py-2 d-flex align-items-center justify-content-center rounded-2 text-nowrap">{item}</p>)
             }
